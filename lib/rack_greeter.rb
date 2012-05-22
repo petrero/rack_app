6984b80d8ae36eeb85b14b92834e47df52ab3129
Petre Diaconescu <petrero@gmail.com>
2012-05-22 20:04:33 +0300

    loading an HTML document in rack application

--- lib/rack_greeter.rb
+++ lib/rack_greeter.rb
@@ -1,5 +1,11 @@
+require "erb"
 class RackGreeter
   def call(env)
-    Rack::Response.new("Hello")
+    Rack::Response.new(render("index.html.erb"))
   end
+end
+
+def render(template)
+  path = File.expand_path("../views/#{template}", __FILE__)
+  ERB.new(File.read(path)).result(binding)
 end
