78c846e9d2ea5b9358b0f140b0890f17b5d737ea
Petre Diaconescu <petrero@gmail.com>
2012-05-22 20:30:43 +0300

    display the name that was assigned in the World column

--- lib/rack_greeter.rb
+++ lib/rack_greeter.rb
@@ -1,20 +1,26 @@
 require "erb"
 class RackGreeter
   def call(env)
-    request = Rack::Request.new(env)
-    case request.path
+    @request = Rack::Request.new(env)
+    case @request.path
     when "/" then Rack::Response.new(render("index.html.erb"))
     when "/change" 
       Rack::Response.new do |response|
-        response.set_cookie("greet", request.params["name"])
+        response.set_cookie("greet", @request.params["name"])
         response.redirect("/")
       end
     else Rack::Response.new("Not Found", 404) 
     end
   end
+  
+  def render(template)
+    path = File.expand_path("../views/#{template}", __FILE__)
+    ERB.new(File.read(path)).result(binding)
+  end
+
+  def greet_name
+    @request.cookies["greet"] || "World" 
+  end
 end
 
-def render(template)
-  path = File.expand_path("../views/#{template}", __FILE__)
-  ERB.new(File.read(path)).result(binding)
-end
+
